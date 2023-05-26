d0e8464ddac40c2c0b200936dc809366c7a92012
Munteanu Teofil <andreiteofil01@gmail.com>
2023-05-26 05:33:18 +0300

    controllers + db init

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+   public function index(){
+       return view('index');
+   }
+   
+   public function login(){
+       return view('login');
+   }
+   
+   public function main(){
+       return view('main');
+   }
 }
